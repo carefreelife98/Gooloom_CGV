c1f7d072a21b56de7173b42e955fd332b6259960
CarefreeLife <csm4903@naver.com>
2023-08-20 15:40:40 +0900

    use template

--- gooloom_terraform/terraform.tfvars
+++
@@ -1,7 +0,0 @@
-region         = "ap-northeast-2"
-vpc_cidr       = "10.0.0.0/16"
-azs            = ["ap-northeast-2a", "ap-northeast-2c"]
-num_azs        = 2
-num_private_subnets = 3
-ami	       = "ami-01056eaaa603955a4"
-kpair	       = "gooloom"
--- gooloom_terraform/variables.tf
+++ gooloom_terraform/variables.tf
@@ -1,36 +1,50 @@
+##############################################################################
+# Variables File
+#
+# Here is where we store the default values for all the variables used in our
+# Terraform code. If you create a variable with no default, the user will be
+# prompted to enter it (or define it via config file or command line flags.)
+
+variable "prefix" {
+  description = "This prefix will be included in the name of most resources."
+}
+
 variable "region" {
-  description = "AWS region"
-  type        = string
+  description = "The region where the resources are created."
+  default     = "us-east-1"
 }
 
-variable "vpc_cidr" {
-  description = "CIDR block for the VPC"
-  type        = string
+variable "address_space" {
+  description = "The address space that is used by the virtual network. You can supply more than one address space. Changing this forces a new resource to be created."
+  default     = "10.0.0.0/16"
 }
 
-variable "azs" {
-  description = "Availability Zones"
-  type        = list(string)
+variable "subnet_prefix" {
+  description = "The address prefix to use for the subnet."
+  default     = "10.0.10.0/24"
 }
 
-variable "num_azs" {
-  description = "Number of Availability Zones"
-  type        = number
-  default     = 2
+variable "instance_type" {
+  description = "Specifies the AWS instance type."
+  default     = "t2.micro"
 }
 
-variable "num_private_subnets" {
-  description = "Number of private subnets per Availability Zone"
-  type        = number
-  default     = 3
+variable "admin_username" {
+  description = "Administrator user name for mysql"
+  default     = "hashicorp"
 }
 
-variable "ami" {
-  description = "Instance AMI"
-  type        = string
+variable "height" {
+  default     = "400"
+  description = "Image height in pixels."
 }
 
-variable "kpair" {
-  description = "gooloom_keypair_name"
-  type	      = string
+variable "width" {
+  default     = "600"
+  description = "Image width in pixels."
+}
+
+variable "placeholder" {
+  default     = "placekitten.com"
+  description = "Image-as-a-service URL. Some other fun ones to try are fillmurray.com, placecage.com, placebeard.it, loremflickr.com, baconmockup.com, placeimg.com, placebear.com, placeskull.com, stevensegallery.com, placedog.net"
 }
