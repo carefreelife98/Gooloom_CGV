f7438a3fb17df320a8d33b4f50d2280573edf3e5
CarefreeLife <csm4903@naver.com>
2023-08-21 01:23:31 +0900

    update main.tf

--- CGV_Gooloom_Terraform_AWS/main.tf
+++ CGV_Gooloom_Terraform_AWS/main.tf
@@ -1,14 +1,31 @@
-provider "aws" {
-  region = var.region
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "=3.42.0"
+    }
+  }
 }
 
-resource "aws_vpc" "main" {
-  cidr_block = var.vpc_cidr
+provider "aws" {
+  region  = var.region
+  access_key = var.AWS_ACCESS_KEY_ID
+  secret_key = var.AWS_SECRET_ACCESS_KEY
+}
+
+resource "aws_vpc" "vpc" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_hostnames = true
+
+  tags = {
+    name = "${var.prefix}-vpc-${var.region}"
+    environment = "Production"
+  }
 }
 
 resource "aws_subnet" "public_subnet" {
   count             = var.num_azs * 2
-  vpc_id            = aws_vpc.main.id
+  vpc_id            = aws_vpc.vpc.id
   cidr_block        = cidrsubnet(var.vpc_cidr, 8, count.index)
   availability_zone = element(var.azs, count.index / 2)
   map_public_ip_on_launch = true
@@ -20,7 +37,7 @@ resource "aws_subnet" "public_subnet" {
 
 resource "aws_subnet" "private_subnet" {
   count             = var.num_azs * 2 * var.num_private_subnets
-  vpc_id            = aws_vpc.main.id
+  vpc_id            = aws_vpc.vpc.id
   cidr_block        = cidrsubnet(var.vpc_cidr, 8, count.index + 8)
   availability_zone = element(var.azs, count.index / (2 * var.num_private_subnets))
 
@@ -29,12 +46,25 @@ resource "aws_subnet" "private_subnet" {
   }
 }
 
-resource "aws_internet_gateway" "gw" {
-  vpc_id = aws_vpc.main.id
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.vpc.id
+
+  tags = {
+    Name = "${var.prefix}-internet-gateway"
+  }
+}
+
+resource "aws_route_table" "rt" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
 }
 
 resource "aws_lb" "alb" {
-  name               = "my-alb"
+  name               = "${var.prefix}-${var.env}-CGV-alb"
   internal           = false
   load_balancer_type = "application"
   subnets            = aws_subnet.public_subnet[*].id
