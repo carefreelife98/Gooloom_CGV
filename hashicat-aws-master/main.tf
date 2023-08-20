ffa186f64d56f46b8320824793a02f1463f3abba
CarefreeLife <csm4903@naver.com>
2023-08-20 17:20:45 +0900

    really done?

--- hashicat-aws-master/main.tf
+++ hashicat-aws-master/main.tf
@@ -8,6 +8,8 @@ terraform {
 }
 
 provider "aws" {
+  shared_config_files      = ["/Users/carefreelife/.aws/config"]
+  shared_credentials_files = ["/Users/carefreelife/.aws/credentials"]
   profile = "default"
   region  = var.region
 }
