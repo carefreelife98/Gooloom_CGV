f6da035f63da6147de8a79e724a92b46050c9a34
CarefreeLife <csm4903@naver.com>
2023-08-21 11:16:53 +0900

    fffiinaaly

--- CGV_Gooloom_Terraform_AWS/main.tf
+++ CGV_Gooloom_Terraform_AWS/main.tf
@@ -104,7 +104,7 @@ resource "aws_route_table" "public_subnet_rt" {
   vpc_id = aws_vpc.vpc.id
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = "aws_internet_gateway.igw.id" #Internet Gateway 별칭 입력
+    gateway_id = aws_internet_gateway.igw.id #Internet Gateway 별칭 입력
   }
   tags = {
     Name = "${var.prefix}-${var.env}-public-subnet-rt-${count.index}"
