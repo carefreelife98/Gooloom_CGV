aeba8ebd8055d1824bf05c50e05a0c92f9560d63
CarefreeLife <csm4903@naver.com>
2023-08-21 11:23:52 +0900

    update

--- CGV_Gooloom_Terraform_AWS/main.tf
+++ CGV_Gooloom_Terraform_AWS/main.tf
@@ -128,15 +128,23 @@ resource "aws_route_table_association" "public_subnet_assoc" {
 
 ############### private routing ###############
 
-#resource "aws_route_table" "private_subnet_rt_2a" {
-#  count = 3
-#  vpc_id = aws_vpc.vpc.id
-#
-#  tags = {
-#    Name = "${var.prefix}-${var.env}-private-subnet-rt-2a-${count.index}"
-#  }
-#}
+resource "aws_route_table" "private_subnet_rt_2a" {
+  count = 3
+  vpc_id = aws_vpc.vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = local #Internet Gateway 별칭 입력
+  }
+  tags = {
+    Name = "${var.prefix}-${var.env}-private-subnet-rt-2a-${count.index}"
+  }
+}
 
+resource "aws_route_table_association" "private_subnet_2a_assoc" {
+  count       = 3
+  subnet_id   = aws_subnet.private_subnet-2a[count.index].id
+  route_table_id = aws_route_table.private_subnet_rt_2a[count.index].id
+}
 #resource "aws_route" "private_subnet_rt_2a_association" {
 #  count = 3
 #  route_table_id         = aws_route_table.private_subnet_rt_2a[count.index].id
@@ -144,20 +152,24 @@ resource "aws_route_table_association" "public_subnet_assoc" {
 #  nat_gateway_id         = aws_instance.nat_instance_2a[count.index].id
 #}
 #
-#resource "aws_route_table_association" "private_subnet_2a_assoc" {
-#  count       = 3
-#  subnet_id   = aws_subnet.private_subnet-2a[count.index].id
-#  route_table_id = aws_route_table.private_subnet_rt_2a[count.index].id
-#}
 
-#resource "aws_route_table" "private_subnet_rt_2c" {
-#  count = 3
-#  vpc_id = aws_vpc.vpc.id
-#
-#  tags = {
-#    Name = "${var.prefix}-${var.env}-private-subnet-rt-2c-${count.index}"
-#  }
-#}
+resource "aws_route_table" "private_subnet_rt_2c" {
+  count = 3
+  vpc_id = aws_vpc.vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = local #Internet Gateway 별칭 입력
+  }
+  tags = {
+    Name = "${var.prefix}-${var.env}-private-subnet-rt-2c-${count.index}"
+  }
+}
+
+resource "aws_route_table_association" "private_subnet_2c_assoc" {
+  count       = 3
+  subnet_id   = aws_subnet.private_subnet-2c[count.index].id
+  route_table_id = aws_route_table.private_subnet_rt_2c[count.index].id
+}
 #
 #resource "aws_route" "private_subnet_rt_2c_association" {
 #  count = 3
@@ -166,11 +178,6 @@ resource "aws_route_table_association" "public_subnet_assoc" {
 #  nat_gateway_id         = aws_instance.nat_instance_2c[count.index].id
 #}
 #
-#resource "aws_route_table_association" "private_subnet_2c_assoc" {
-#  count       = 3
-#  subnet_id   = aws_subnet.private_subnet-2c[count.index].id
-#  route_table_id = aws_route_table.private_subnet_rt_2c[count.index].id
-#}
 
 
 ############### NAT Instance & Keypair ###############
